e3c07231f1df1fcffcea31e17ec07a38b14aa768
Nazmul Huda <nznazmulhuda04@gmail.com>
2024-06-05 02:09:20 +0600

    My Employee list HR page added

--- src/Pages/HRManager/MyEmployeeList/MyEmployeeList.jsx
+++ src/Pages/HRManager/MyEmployeeList/MyEmployeeList.jsx
@@ -1,10 +1,28 @@
+import Team from "../../../Components/MyEmployee/Team";
+import Heading from "../../../Components/Shared/Heading";
+
 function MyEmployeeList() {
     return (
-        <>
-            <div>
-                <h1>MyEmployeeList</h1>
+        <div className="container mx-auto">
+            <Heading
+                title={"Your Employee List"}
+                subTitle={"all of your team member"}
+            />
+
+            <div className="flex items-center md:gap-16 justify-center flex-wrap mt-5">
+                <Team
+                    memberName={"Nazmul Huda"}
+                    memberPic={"https://i.ibb.co/HGMZbc7/4.jpg"}
+                    memberType={"Employee"}
+                />
+
+                <Team
+                    memberName={"Nazmul Huda"}
+                    memberPic={"https://i.ibb.co/HGMZbc7/4.jpg"}
+                    memberType={"Admin"}
+                />
             </div>
-        </>
+        </div>
     );
 }
 
